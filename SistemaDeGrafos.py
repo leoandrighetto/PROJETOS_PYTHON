985e000aff9ec915282989b0fa7cf4816902c998
Leonardo Andrighetto <leoandrighetto02@gmail.com>
2025-06-06 13:32:54 -0300

    Update SistemaDeGrafos.py

--- SistemaDeGrafos.py
+++ SistemaDeGrafos.py
@@ -20,65 +20,97 @@ A entrada de dados deve ser feita por interface e também por um arquivo csv uni
 Porto Alegre, Pelotas, 291.3km
 '''
 
+
 class Grafos:
 
     def __init__(self):
         self.cidades = []
-        self.conexos = []
+        self.conexoes = []
 
-    def Linha(self):
-        print(34*"*")
+    def menu(self):
 
-    def Menu(self):
-        Grafos.Linha(self)
-        print(f'BEM-VINDO ao meu sistema de Grafos\n')
-        print(f'Digite a opção desejada:\n')
-        print('1 - Cadastrar cidade')
-        print('2 - Cadastrar conexão')
-        print('3 - Listar cidades')
-        print('4 - Listar conexões')
-        print('5 - Listar cidades vizinhas\n')
+        print(f'*** Sistema de Grafos ***\n\n'
+              '1 - Cadastrar cidade\n'
+              '2 - Cadastrar conexão\n'
+              '3 - Listar cidades\n'
+              '4 - Listar conexões\n'
+              '5 - Listar cidades vizinhas\n')
 
-    def CadastrarCidade(self):
+        while True:
+
+            try:
+
+                escolha_usuario = int(input(f'Digite a opção desejada: '))
+
+                match escolha_usuario:
+                    case 1:
+                        Grafos().cadastrar_cidade()
+
+                    case 2:
+                        Grafos().cadastrar_conexao()
+
+                    case 3:
+                        Grafos().info_cidades()
+
+                    case 4:
+                        Grafos().info_conexoes()
+
+                    case 5:
+                        Vertices().info_vizinhos()
+
+            except ValueError:
+                print('\nEntrada inválida, digite apenas números.\n')
+
+    def cadastrar_cidade(self):
+        print('\n*** Cadastro de cidades ***\n')
+
+        while True:
+
+            nome_cidade = input('Digite o nome da cidade: \n')
+            self.cidades.append(nome_cidade)
+
+            pergunta_usuario = input('Deseja cadastrar mais cidades (s/n) ? \n')
+
+            while True:
+                if pergunta_usuario.lower() == "s":
+                    break
+
+                else:
+                    Grafos().menu()
+
+    def cadastrar_conexao(self):
         pass
 
-    def CadastrarConexao(self):
+    def info_cidades(self):  # 3 - Listar Cidades
         pass
 
-    def infoCidades(self): # 3 - Listar Cidades
-        pass
-
-    def infoConexoes(self): # 4 - Listas conexões
+    def info_conexoes(self):  # 4 - Listas conexões
         pass
 
 
 class Vertices:
 
-    def __init__(self):
-
+    def __init__(self,nome_cidade,vizinhanca,conexoes):
         self.nome_cidade = None
         self.vizinhanca = []
-        self.conexoes = []
+        self.conexoes = conexoes
 
+    def info_vizinhos(self): pass
 
-    def infoVizinhos(self): pass
+    def info_conexoes(self): pass
 
-    def infoConexoes(self): pass
-
-    def infoVertice(self): pass
+    def info_vertice(self): pass
 
 
 class Arestas:
 
-    def __init__(self):
-        self.cidade1 = self.nome_cidade
-        self.cidade2 = self.nome_cidade
-        self.distancia = None
+    def __init__(self, cidade1, cidade2, distancia=0):
+        self.cidade1 = cidade1
+        self.cidade2 = cidade2
+        self.distancia = distancia
 
-    def infoAresta(self): pass
-
-
+    def info_aresta(self): pass
 
 
 if __name__ == '__main__':
-    Grafos().Menu()
+    Grafos().menu()
